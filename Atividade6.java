c39083d1a206a4778715a676ad70dbd45fdf04d2
Hermes Feitoza Barros <57917124+WinterFB@users.noreply.github.com>
2022-10-05 20:31:12 -0300

    Create Atividade6.java

---
+++ Atividade6.java
@@ -0,0 +1,14 @@
+/**
+ * Questão 06 - Atividade Colaborativa (2.03)
+ * 
+ * @author Hermes Feitoza Barros 
+ * @version 05/10/2022
+ */
+
+class Atividade6
+{
+    public static void main(String[] args){
+        System.out.println("Hermes Feitoza Barros");
+        System.out.println("Seleção brasileira.");
+    }
+}
